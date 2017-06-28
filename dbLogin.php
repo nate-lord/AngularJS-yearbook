ab1f16ab1ff078f4fff00f03e60965918564e873
Nate Lord <nate-lord@users.noreply.github.com>
2017-06-28 01:31:57 -0400

    Update dbLogin.php

--- dbLogin.php
+++ dbLogin.php
@@ -1,19 +1,9 @@
 <?php
 function dbLogin() {
-  /*
-  // live
 	return array( 
-		'dsn'	 => 'mysql:dbname=natelord_gug;host=localhost;port=8888',
-		'user' => 'natelord_gug_usr',
-		'pass' => '+Lszfyy&2ql]'
-	);
-  */
-  
-  // local
-	return array( 
-		'dsn'	 => 'mysql:dbname=guggenheim;host=localhost;port=8888',
-		'user' => 'natelord_gug_usr',
-		'pass' => '+Lszfyy&2ql]'
+		'dsn'	 => 'mysql:dbname=XXXXXXXXXX;host=localhost;port=8888',
+		'user' => 'XXXXXXXXXX',
+		'pass' => 'XXXXXXXXXX'
 	);
 }
 ?>
